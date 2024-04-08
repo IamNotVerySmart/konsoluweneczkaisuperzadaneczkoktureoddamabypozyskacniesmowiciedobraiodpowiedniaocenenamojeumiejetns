d966aa5e786f987974b90c0811cb2ebb1a4c60c9
IamNotVerySmart <93706080+IamNotVerySmart@users.noreply.github.com>
2024-04-08 10:11:45 +0200

    :thumbs up:

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] tablica = new int[101];
+Random random = new Random();
+for (int i = 0; i < tablica.Length; i++)
+{
+    tablica[i] = random.Next(0, 101);
+}
+Console.WriteLine("Podaj szukaną wartość: ");
+int a = int.Parse(Console.ReadLine());
+tablica[tablica.Length - 1] = a;
+int indeks = 0;
+for (int i = 0; i < tablica.Length; i++)
+{
+    if (tablica[i] == a)
+    {
+        indeks = i;
+        break;
+    }
+}
+Console.WriteLine("Liczba szukana znaleziona na indeksie: " + indeks);
+foreach (int i in tablica)
+{
+    Console.Write(i + ", ");
+}
